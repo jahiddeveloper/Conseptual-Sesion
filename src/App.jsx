043bb4991dfc56a436a49b82535305186bb70160
jahiddeveloper <md.jahidulislam.mar@gmail.com>
2025-12-04 14:29:49 +0600

    Footer section is compleat

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Footer from './Footer'
 import Navbar from './Navbar'
 
 function App() {
@@ -6,6 +7,7 @@ function App() {
   return (
     <>
       <Navbar></Navbar>
+      <Footer></Footer>
     </>
   )
 }
