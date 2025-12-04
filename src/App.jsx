80cf242e82aabfcc8c04b3c10cf249039eb9564e
jahiddeveloper <md.jahidulislam.mar@gmail.com>
2025-12-04 15:09:21 +0600

    Button section is compleat

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,17 @@ function App() {
     <>
       <Navbar></Navbar>
       <Hero></Hero>
+
+        <div className='text-right container mx-auto mt-5 md:mt-10 mb-5 md:mb-10'>
+          <button className='text-purple-500 bg-purple-100 font-bold text-lg px-6 py-2 rounded-l-lg border-r-2 border-purple-500 cursor-pointer'>All</button>
+
+          <button className='text-purple-500 bg-purple-100 font-bold text-lg px-6 py-2 border-r-2 border-purple-500 cursor-pointer'>Pending</button>
+
+          <button className='text-purple-500 bg-purple-100 font-bold text-lg px-6 py-2 border-r-2 border-purple-500 cursor-pointer'>Submited</button>
+
+          <button className='text-purple-500 bg-purple-100 font-bold text-lg px-6 py-2 rounded-r-lg cursor-pointer'>Reviewed</button>
+        </div>
+
       <Footer></Footer>
     </>
   )
