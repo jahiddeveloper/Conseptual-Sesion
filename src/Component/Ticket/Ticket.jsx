a443386d4c4b05282325eb04ae831168b69df9e1
jahiddeveloper <md.jahidulislam.mar@gmail.com>
2025-12-07 07:49:48 +0600

    Card loading compleat

--- src/Component/Ticket/Ticket.jsx
+++ src/Component/Ticket/Ticket.jsx
@@ -1,11 +1,38 @@
-import React from 'react';
+import React from "react";
+import calender from "../../assets/calender.png";
 
-const Ticket = () => {
-    return (
-        <div>
-            
+const Ticket = ({ ticket }) => {
+  return (
+    <div className="font">
+      <div className="bg-white p-4 shadow-sm rounded-xl cursor-pointer hover:scale-103 hover:transition duration-400">
+        <div className="flex justify-between items-center">
+          <h1 className="text-xl font-bold">{ticket.title}</h1>
+          <button
+            className={`flex items-center gap-2 cursor-pointer px-5 py-2 rounded-4xl font-semibold bg-[#b9f8cf]`}
+          >
+            {ticket.status}
+          </button>
         </div>
-    );
+        <p className="mt-3 text-gray-500 w-full md:w-125">
+          {ticket.description}
+        </p>
+
+        <div className="flex justify-between items-center mt-3">
+          <div className="flex items-center gap-4">
+            <p className="text-gray-500 font-semibold">#{ticket.id}</p>
+            <p className={``}>{ticket.priority}</p>
+          </div>
+          <div className="flex items-center gap-4">
+            <p>{ticket.customer}</p>
+            <div className="flex items-center gap-3">
+              <img src={calender} alt="" />
+              <p>{ticket.date}</p>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Ticket;
--- src/Component/Tickets/Tickets.jsx
+++ src/Component/Tickets/Tickets.jsx
@@ -1,51 +1,16 @@
 import React, { use } from "react";
-import calender from "../../assets/calender.png";
+import Ticket from "../Ticket/Ticket";
 
 const Tickets = ({ fetchTicket }) => {
   let loadTicket = use(fetchTicket);
-  console.log(loadTicket);
 
   return (
-    <div className="font container mx-auto">
-      <div>
-        <div className="bg-white p-4 shadow-sm rounded-xl">
-          <div className="flex justify-between items-center">
-            <h1 className="text-xl font-bold">Jahid</h1>
-            <button
-              className={`flex items-center gap-2 cursor-pointer px-5 py-2 rounded-4xl font-semibold bg-[#b9f8cf]`}
-            >
-                Hello
-            </button>
-          </div>
-          <p className="mt-3 text-gray-500 w-full md:w-125">
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Saepe, aperiam.
-          </p>
+    <div className="font container mx-auto grid grid-cols-1 md:grid-cols-2 gap-5">
+        
+        {
+            loadTicket.map(ticket => <Ticket ticket = {ticket}></Ticket>)
+        }
 
-          <div className="flex justify-between items-center mt-3">
-            <div className="flex items-center gap-4">
-              <p className="text-gray-500 font-semibold">#10001</p>
-              <p
-                className={`${
-                  loadTicket.priority == "HIGH PRIORITY"
-                    ? "text-red-500"
-                    : loadTicket.priority == "MEDIUM PRIORITY"
-                    ? "text-[#febb0c]"
-                    : "text-[#02a53b]"
-                }`}
-              >
-                High priority
-              </p>
-            </div>
-            <div className="flex items-center gap-4">
-              <p>Jahid</p>
-              <div className="flex items-center gap-3">
-                <img src={calender} alt="" />
-                <p>20025</p>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
     </div>
   );
 };
